ac81cf8809fdd84446c2a0c807baaf8b6aa9f45b
emmacona <emmanuelle.coutu-nadeau@mail.mcgill.ca>
2021-09-18 15:58:20 -0400

    Nursery dataset

---
+++ dataset_1_preprocessing.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpt 
+
+# Initializing datasets (readers)
+dataset1 = pd.read_csv('adult.csv')
--- preprocessing.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import matplotlib as mpt 
-import pandas as pd
-
-# Initializing datasets (readers)
-dataset1 = pd.read_csv('adult.csv')
-dataset2 = pd.read_csv('winequality-red.csv')
