137d140a638e042cfee5ec3875c1e25691d5ec1e
emmacona <emmanuelle.coutu-nadeau@mail.mcgill.ca>
2021-09-18 15:08:22 -0400

    Preprocessing setup and datasets

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,7 @@
 import numpy as np
+import matplotlib as mpt 
+import pandas as pd
+
+# Initializing datasets (readers)
+dataset1 = pd.read_csv('adult.csv')
+dataset2 = pd.read_csv('winequality-red.csv')
